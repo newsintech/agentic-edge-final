9fe6aa12069a465fbcb8bf1d1902594f34d4535d
newsintech <mfry380@duck.com>
2026-01-07 22:46:04 +0530

    Delete app/lib directory

--- app/lib/geo.ts
+++
@@ -1,7 +0,0 @@
-// lib/geo.ts
-export const detectCountryFromIP = (): string => {
-  // Use Vercel header for country if deployed
-  const countryCode = process.env.VERCEL_COUNTRY || 'US';
-  const supportedCountries = ['IN', 'US', 'UK', 'CA', 'DE', 'FR', 'IT', 'ES', 'NL'];
-  return supportedCountries.includes(countryCode) ? countryCode : 'US';
-};
