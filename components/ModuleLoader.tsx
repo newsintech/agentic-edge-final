be5128e202f23aff87447addbe857f3ec1bfc708
newsintech <mfry380@duck.com>
2026-01-08 13:25:21 +0530

    Update ModuleLoader.tsx

--- components/ModuleLoader.tsx
+++ components/ModuleLoader.tsx
@@ -1,10 +1,23 @@
-import Hero from "@/modules/01-hero/hero";
+'use client'
 
-export default function ModuleLoader() {
-  return (
-    <>
-      <Hero />
-    </>
-  );
+import React from 'react'
+
+interface ModuleLoaderProps {
+  modules: string[]
 }
 
+export default function ModuleLoader({ modules }: ModuleLoaderProps) {
+  return (
+    <div className="min-h-screen">
+      {modules.map((modulePath, index) => {
+        try {
+          const Module = require(`@/modules/${modulePath}`).default
+          return <Module key={index} />
+        } catch (error) {
+          console.error(`Failed to load module: ${modulePath}`, error)
+          return null
+        }
+      })}
+    </div>
+  )
+}
