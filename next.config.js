94461fc7382f1b0ea75d5e3d21f3b7f071056fc6
newsintech <mfry380@duck.com>
2026-01-08 13:04:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
-    appDir: true,
+    typedRoutes: true, 
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
 module.exports = nextConfig;
-
