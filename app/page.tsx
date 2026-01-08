97f7b85abc09d992b4196c1acc0f522483457d28
newsintech <mfry380@duck.com>
2026-01-08 13:11:38 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import ModuleLoader from "@/components/ModuleLoader";
+
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>AgenticEdge Home</h1>
-      <p>If this loads, routing is fixed.</p>
-    </div>
+    <main>
+      <ModuleLoader />
+    </main>
   );
 }
