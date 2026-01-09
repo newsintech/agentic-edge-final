417ba2ac69b75414310138dd5b542292b7de2ac9
newsintech <mfry380@duck.com>
2026-01-09 22:16:02 +0530

    Update PrivacyTools.ts

--- modules/PrivacyTools.ts
+++ modules/PrivacyTools.ts
@@ -1,1 +1,12 @@
+// modules/PrivacyTools.ts
+export interface PrivacyTool {
+  name: string;
+  affiliateLink?: string;
+}
+
+export const PrivacyTools: PrivacyTool[] = [
+  { name: 'ProtonVPN', affiliateLink: 'https://protonvpn.com?affiliate=yourID' },
+  { name: 'NordVPN', affiliateLink: 'https://nordvpn.com?affiliate=yourID' },
+  { name: 'Tails OS', affiliateLink: undefined },
+];
 
